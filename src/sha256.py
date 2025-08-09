af6ef1e845592f7a0083c6b38639ba0e1f7a823e
Drew <145530440+DrewRoss5@users.noreply.github.com>
2025-08-08 23:33:39 -0600

    Add support for multiple rounds of hashing

--- src/sha256.py
+++ src/sha256.py
@@ -1,6 +1,8 @@
 from Crypto.Hash import SHA256
 
-def sha256_hash(plaintext: bytes):
+def sha256_hash(plaintext: bytes, rounds: int = 1):
     sha_hash = SHA256.new()
-    sha_hash.update(plaintext)
-    return sha_hash.digest()
+    for i in range(rounds):
+        sha_hash.update(plaintext)
+        plaintext = sha_hash.digest()
+    return plaintext
